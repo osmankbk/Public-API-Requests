de2916fb96519482238066c2dca71fb8bfad977e
Ozman K <mackiekbk@yahoo.com>
2019-09-14 12:23:18 -0400

    wrote the modal window code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,31 @@
 const urlRequest = 'https://randomuser.me/api/?results=12';
+const urlIndividual = 'https://randomuser.me/api';
 const galleryDiv = document.getElementById('gallery');
 const body = document.querySelector('body');
 
+/*const elementCreate = (elementName, property, value) => {
+  const element = document.createElement(elementName);
+  element[property] = value;
+  return element;
+}*/
+
+async function shortCutRequest(url) {
+  try {
+    const request = await fetch(url);
+    return await request.json();
+  }
+  catch {
+
+  }
+}
 
 
+async function fetchReques(url) {
 
-
-async function fetchRequest(url) {
-  const request = await fetch(url);
-  const response = await request.json();
-
+  const response = await shortCutRequest(url);
+  console.log(response);
   const employeeResponse = response.results.map(user => {
-    console.log(user);
+
 
     //const containerDiv = elementCreate('div', 'class', 'card');
     //const imageDiv = elementCreate('div', 'class', 'card-img-container');
@@ -25,20 +39,20 @@ async function fetchRequest(url) {
     containerDiv.appendChild(imageDiv);
     containerDiv.appendChild(infoDiv);
     galleryDiv.appendChild(containerDiv);
-    imageDiv.innerHTML = `<img class="card-img" src=${user.picture.large} alt="profile picure">`;
 
+    imageDiv.innerHTML = `<img class="card-img" src=${user.picture.large} alt="profile picure">`;
     infoDiv.innerHTML = `<h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
       <p class="card-text">${user.email}</p>
       <p class="card-text cap">${user.location.city}</p>`;
   });
 }
-/*const elementCreate = (elementName, property, value) => {
-  const element = document.createElement(elementName);
-  element[property] = value;
-  return element;
-}*/
 
-const modelWindow = () => {
+
+async function modelWindow(url) {
+  const perInfoRequest = await shortFetchRequest(url);
+
+  const perInfoResponse = perUserFetch.results.map(user => {
+
 const greatGranpaModal = document.createElement('div');
 const granpaModal = document.createElement('div');
 const button = document.createElement('button');
@@ -46,18 +60,28 @@ const fatherModal = document.createElement('div');
 greatGranpaModal.classList.add('modal-container');
 granpaModal.classList.add('modal');
 button.classList.add('modal-close.btn');
+fatherModal.classList('modal-info-container');
+body.appendChild(greatGranpaModal);
 greatGranpaModal.appendChild(granpaModal);
 granpaModal.appendChild(button);
 granpaModal.appendChild(fatherModal);
-button.innerHTML = `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
-fatherModal.innerHTML = `<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-<h3 id="name" class="modal-name cap">name</h3>
-<p class="modal-text">email</p>
-<p class="modal-text cap">city</p>
-<hr>
-<p class="modal-text">(555) 555-5555</p>
-<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-<p class="modal-text">Birthday: 10/21/2015</p>`;
 
+button.innerHTML = `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
+fatherModal.innerHTML = `<img class="modal-img" src=${user.picture.large} alt="profile picture">
+<h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+<p class="modal-text">${user.email}</p>
+<p class="modal-text cap">${user.location.city}</p>
+<hr>
+<p class="modal-text">${user.phone}</p>
+<p class="modal-text">${user.location.street} ${user.location.city} ${user.location.state}</p>
+<p class="modal-text">Birthday: ${user.dob.date}</p>`;
+
+})
 }
-console.log(fetchRequest(urlRequest));
+
+window.addEventListener('click', (e) => {
+  if(e.target.CLASSNAME === 'modal-container'){
+    modelWindow(urlIndividual);
+  }
+});
+fetchReques(urlRequest);

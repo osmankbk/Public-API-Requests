410cb26eded880e32bb81318d2099ca2689e7b42
Ozman K <mackiekbk@yahoo.com>
2019-09-19 16:01:43 -0400

    changed the format of the code. from 1 function to multiple

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-const urlRequest = 'https://randomuser.me/api/?results=12';
+const urlRequest = 'https://randomuser.me/api/?results=12&nat=us,ca';
 const galleryDiv = document.getElementById('gallery');
 const body = document.querySelector('body');
+
+//Search section elements
 const searchDiv = document.querySelector('.search-container');
+searchDiv.innerHTML = `<form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`;
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-submit');
 
 //DRY function for creating elements
   const elementCreate = (elementName, property, value) => {
@@ -10,103 +18,106 @@ const searchDiv = document.querySelector('.search-container');
   return element;
 }
 
-//Search section elements
-const form = document.createElement('form');
-const input = elementCreate('input', 'className', 'search-input');
-const secondInput = elementCreate('input', 'className', 'search-submit');
-form.action = "#";
-form.method = "get";
-input.type = 'search';
-input.placeholder = "Search...";
-secondInput.type = 'submit';
-form.appendChild(input);
-form.appendChild(secondInput);
-searchDiv.appendChild(form);
+
+
+
 
 async function shortCutRequest(url) {
   try {
     const request = await fetch(url);
     return await request.json();
   }
-  catch {
-
+  catch (error){
+    gallery.innerHTML = `An error occured fetching the data, ${error}`;
   }
 }
 
-
 async function fetchRequest(url) {
+  try {
+    const response = await shortCutRequest(url);
+    const employeeResponse = response.results.forEach(async (user) => {
 
-  const response = await shortCutRequest(url);
-  const employeeResponse = response.results.forEach(user => {
+  //display randomuser section
+      const containerDiv = elementCreate('div', 'className', 'card');
+      galleryDiv.appendChild(containerDiv);
+      containerDiv.innerHTML = `<div class="card-img-container">
+          <img class="card-img" src=${user.picture.large} alt="profile picture">
+      </div>
+      <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+          <p class="card-text">${user.email}</p>
+          <p class="card-text cap">${user.location.city}</p>
+      </div>`;
 
-//display randomuser section
-    const containerDiv = elementCreate('div', 'className', 'card');
-    const imageDiv = elementCreate('div', 'className', 'card-img-container');
-    const infoDiv = elementCreate('div', 'className', 'card-info-container');
-
-    containerDiv.appendChild(imageDiv);
-    containerDiv.appendChild(infoDiv);
-    galleryDiv.appendChild(containerDiv);
-
-    imageDiv.innerHTML = `<img class="card-img" src=${user.picture.large} alt="profile picure">`;
-    infoDiv.innerHTML = `<h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
-      <p class="card-text">${user.email}</p>
-      <p class="card-text cap">${user.location.city}</p>`;
-
-//modal window section
-      containerDiv.addEventListener('click', (e) => {
-
-        const modalContainer =  elementCreate('div', 'className', 'modal-container');
-        const modal = elementCreate('div', 'className', 'modal');
-        const modalInfo = elementCreate('div', 'className', 'modal-info-container');
-        const modalButtonContainer = elementCreate('div', 'className', 'modal-btn-container');
-        const modalPrevButton = elementCreate('button', 'className', 'modal-prev btn');
-        const modalNextButton = elementCreate('button', 'className', 'modal-next btn');
-        const modalCloseButton = elementCreate('button', 'className', 'modal-close-btn');
-
-        body.appendChild(modalContainer);
-        modalContainer.appendChild(modal);
-        modalContainer.appendChild(modalButtonContainer);
-        modalButtonContainer.appendChild(modalPrevButton);
-        modalButtonContainer.appendChild(modalNextButton);
-        modal.appendChild(modalCloseButton);
-        modal.appendChild(modalInfo);
-        modalPrevButton.textContent = 'Prev';
-        modalNextButton.textContent = 'Next';
-        modalCloseButton.innerHTML = `<strong>X</strong>`;
-        modalInfo.innerHTML = `<img class="modal-img" src=${user.picture.large} alt="profile picture">
-        <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-        <p class="modal-text">${user.email}</p>
-        <p class="modal-text cap">${user.location.city}</p>
-        <hr>
-        <p class="modal-text">${user.phone}</p>
-        <p class="modal-text">${user.location.street} ${user.location.city} ${user.location.state}</p>
-        <p class="modal-text">Birthday: ${user.dob.date}</p>`;
-        modalCloseButton.addEventListener('click', (e) => {
-          modalContainer.remove();
-        })
-
-//Next & prev button section
-        modalNextButton.addEventListener('click', (e) =>{
-          
-        })
-
-        modalPrevButton.addEventListener('click', () =>{
-
-        })
-
+        containerDiv.addEventListener('click', () =>{
+          modalWindow(user);
       })
-//search user section
-    secondInput.addEventListener('click', (e) =>{
-
-      if(input.value === user.name.first || input.value === user.name.last){
-        containerDiv.style.display = 'inline';
-      } else {
-        containerDiv.style.display = 'none';
+      searchButton.addEventListener('click', (e) =>{
+        if(searchInput.value === user.name.first || searchInput.value === user.name.last){
+          containerDiv.style.display = 'inline';
+        }else {
+          containerDiv.style.display = 'none';
+        }
+      })
+      body.addEventListener('keyup', () =>{
+        const cards = document.querySelectorAll('.card');
+        if(searchInput.value === ''){
+        cards.forEach(card => {
+          card.style.display = 'flex';
+        })
       }
-    })
+      })
+      })
+  }
+  catch(error) {
+    gallery.innerHTML = `An error occured getting employee profile, ${error}`;
+  }
+
+}
+
+const modalWindow = (data, i) =>{
+
+  const modalContainer =  elementCreate('div', 'className', 'modal-container');
+  body.appendChild(modalContainer);
+  modalContainer.innerHTML = `<div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+      <img class="modal-img" src=${data.picture.large} alt="profile picture">
+      <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+      <p class="modal-text">${data.email}</p>
+      <p class="modal-text cap">${data.location.city}</p>
+      <hr>
+      <p class="modal-text">${data.phone}</p>
+      <p class="modal-text">${data.location.street} ${data.location.city} ${data.location.state}</p>
+      <p class="modal-text">Birthday: ${data.dob.date}</p>
+      </div>
+  </div>
+
+  // Prev & next button section
+  <div class="modal-btn-container">
+      <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+      <button type="button" id="modal-next" class="modal-next btn">Next</button>
+  </div>`;
+  const close = document.getElementById('modal-close-btn');
+  close.addEventListener('click', () =>{
+    modalContainer.remove();
   })
-};
+  const modals = document.querySelectorAll('.modal-container');
+  const employee = document.querySelectorAll('.card');
+
+  const prevButton = document.querySelector('#modal-prev');
+  const nextButton = document.querySelector('#modal-next');
+
+  prevButton.addEventListener('click', () =>{
+
+  });
+
+   nextButton.addEventListener("click", () => {
+
+})
+}
+
+
 
 
 

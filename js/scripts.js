8b806ed8d2213d6e0ebb040dfc39afd1b3d0a10c
Ozman K <mackiekbk@yahoo.com>
2019-09-22 17:40:41 -0400

    added functionality for the prev & next button

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,25 @@ const urlRequest = 'https://randomuser.me/api/?results=12&nat=us,ca';
 const galleryDiv = document.getElementById('gallery');
 const body = document.querySelector('body');
 
+
+//DRY function for creating elements
+  const elementCreate = (elementName, property, value) => {
+  const element = document.createElement(elementName);
+  element[property] = value;
+  return element;
+}
+//fetch
+async function fetchRequest(url) {
+  try {
+    const request = await fetch(url);
+    const response = await request.json();
+    return Promise.all(response.results);
+  }
+  catch (error){
+    gallery.innerHTML = `An error occured fetching the data, ${error}`;
+  }
+}
+
 //Search section elements
 const searchDiv = document.querySelector('.search-container');
 searchDiv.innerHTML = `<form action="#" method="get">
@@ -11,32 +30,9 @@ searchDiv.innerHTML = `<form action="#" method="get">
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('serach-submit');
 
-//DRY function for creating elements
-  const elementCreate = (elementName, property, value) => {
-  const element = document.createElement(elementName);
-  element[property] = value;
-  return element;
-}
 
-
-
-
-
-async function shortCutRequest(url) {
-  try {
-    const request = await fetch(url);
-    return await request.json();
-  }
-  catch (error){
-    gallery.innerHTML = `An error occured fetching the data, ${error}`;
-  }
-}
-
-async function fetchRequest(url) {
-  try {
-    const response = await shortCutRequest(url);
-    const employeeResponse = response.results.forEach(async (user) => {
-
+const employeesInfoRequest = (data) =>{
+    data.forEach(user => {
   //display randomuser section
       const containerDiv = elementCreate('div', 'className', 'card');
       galleryDiv.appendChild(containerDiv);
@@ -49,83 +45,94 @@ async function fetchRequest(url) {
           <p class="card-text cap">${user.location.city}</p>
       </div>`;
 
-        containerDiv.addEventListener('click', () =>{
-          modalWindow(user);
-      })
-      searchButton.addEventListener('click', (e) =>{
-        if(searchInput.value === user.name.first || searchInput.value === user.name.last){
-          containerDiv.style.display = 'inline';
-        }else {
-          containerDiv.style.display = 'none';
-          //galleryDiv.innerHTML = `<h3>The searched name is not in this list of employees<h3>`;
-        }
-      })
-      body.addEventListener('keyup', () =>{
-        const cards = document.querySelectorAll('.card');
-        if(searchInput.value === ''){
-        cards.forEach(card => {
-          card.style.display = 'flex';
-        })
-      }
-      })
-      })
-  }
-  catch(error) {
-    gallery.innerHTML = `An error occured getting employee profile, ${error}`;
-  }
-
-}
-
-const modalWindow = (data, i) =>{
-
   const modalContainer =  elementCreate('div', 'className', 'modal-container');
   body.appendChild(modalContainer);
   modalContainer.innerHTML = `<div class="modal">
       <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
       <div class="modal-info-container">
-      <img class="modal-img" src=${data.picture.large} alt="profile picture">
-      <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
-      <p class="modal-text">${data.email}</p>
-      <p class="modal-text cap">${data.location.city}</p>
+      <img class="modal-img" src=${user.picture.large} alt="profile picture">
+      <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+      <p class="modal-text">${user.email}</p>
+      <p class="modal-text cap">${user.location.city}</p>
       <hr>
-      <p class="modal-text">${data.phone}</p>
-      <p class="modal-text">${data.location.street} ${data.location.city} ${data.location.state}</p>
-      <p class="modal-text">Birthday: ${data.dob.date}</p>
+      <p class="modal-text">${user.phone}</p>
+      <p class="modal-text">${user.location.street} ${user.location.city} ${user.location.state}</p>
+      <p class="modal-text">Birthday: ${user.dob.date}</p>
       </div>
   </div>
-
-
   <div class="modal-btn-container">
       <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
       <button type="button" id="modal-next" class="modal-next btn">Next</button>
   </div>`;
+  modalContainer.style.display = 'none';
 
-  //Modal Close button
-  const close = document.getElementById('modal-close-btn');
-  close.addEventListener('click', () =>{
-    modalContainer.remove();
+  containerDiv.addEventListener('click', () =>{
+    modalContainer.style.display = 'block';
   })
-  //Prev and next section
-  const modals = document.querySelectorAll('.modal-container');
-  const employee = document.querySelectorAll('.card');
-
-  const prevButton = document.querySelector('#modal-prev');
-  const nextButton = document.querySelector('#modal-next');
-
-  prevButton.addEventListener('click', () =>{
+  modalContainer.addEventListener('click', (e) =>{
+    if(e.target.className === '.modal-close-btn' || e.target.textContent === 'X'){
+      modalContainer.remove();
+    }
+  })
+  searchButton.addEventListener('click', (e) =>{
+    if(searchInput.value === user.name.first || searchInput.value === user.name.last){
+      containerDiv.style.display = 'flex';
+    }else {
+      containerDiv.style.display = 'none';
+    }
 
   });
+  body.addEventListener('keyup', () =>{
+    const cards = document.querySelectorAll('.card');
+    if(searchInput.value === ''){
+    cards.forEach(card => {
+      card.style.display = 'flex';
+    })
+  }
+  });
 
-   nextButton.addEventListener("click", () => {
-
+return data;
 })
 }
 
 
 
 
+const modalInteraction = (data) =>{
+  const cards = document.querySelectorAll('.card');
+  const modalContainers = document.querySelectorAll('.modal-container');
+  const prevButtons = document.querySelectorAll('.modal-prev');
+  const nextButtons = document.querySelectorAll('.modal-next');
+  const arrayOfModals = [...modalContainers];
+  let counter = 0;
+console.log(modalContainers);
+prevButtons.forEach(prev => {
+  prev.addEventListener('click', (e) =>{
+    arrayOfModals[counter].style.display = 'none';
+    if(counter !== 0){
+      arrayOfModals[--counter].style.display = 'block';
+    }else {
+      counter = 11;
+      arrayOfModals[counter].style.display = 'block';
+    }
+  })
+
+})
+nextButtons.forEach(next => {
+  next.addEventListener('click', (e) =>{
+    arrayOfModals[counter].style.display = 'none';
+    if(counter !== 11){
+      arrayOfModals[++counter].style.display = 'block';
+    }else {
+      counter = 0;
+      arrayOfModals[counter].style.display = 'block';
+    }
+  })
+
+})
+}
 
 
-
-
-fetchRequest(urlRequest);
+fetchRequest(urlRequest)
+.then(employeesInfoRequest)
+.then(modalInteraction);

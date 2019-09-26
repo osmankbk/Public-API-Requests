6e6b09cc1d2ebfeb8ad656403c216964aadb3830
Ozman K <mackiekbk@yahoo.com>
2019-09-26 19:09:25 -0400

    I rewrote my search events to be more dynamic as you search

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ async function fetchRequest(url) {
 const employeesInfoRequest = (data) => {
 	data.forEach(user => {
 		const containerDiv = document.createElement('div');
-    containerDiv.classList.add('card');
+		containerDiv.classList.add('card');
 		galleryDiv.appendChild(containerDiv);
 		containerDiv.innerHTML = `<div class="card-img-container">
           <img class="card-img" src=${user.picture.large} alt="profile picture">
@@ -39,9 +39,9 @@ const employeesInfoRequest = (data) => {
           <p class="card-text">${user.email}</p>
           <p class="card-text cap">${user.location.city}</p>
       </div>`;
-//Modal section
+		//Modal section
 		const modalContainer = document.createElement('div');
-    modalContainer.classList.add('modal-container');
+		modalContainer.classList.add('modal-container');
 		body.appendChild(modalContainer);
 		modalContainer.innerHTML = `<div class="modal">
       <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -61,11 +61,11 @@ const employeesInfoRequest = (data) => {
       <button type="button" id="modal-next" class="modal-next btn">Next</button>
   </div>`;
 		modalContainer.style.display = 'none';
-//The click event that displays a modal window fired
+		//The click event that displays a modal window fired
 		containerDiv.addEventListener('click', (e) => {
 			modalContainer.style.display = 'block';
 		})
-//The close click event that closes the the modal window
+		//The close click event that closes the the modal window
 		modalContainer.addEventListener('click', (e) => {
 			const modalContainers = document.querySelectorAll('.modal-container');
 			if (e.target.className === '.modal-close-btn' || e.target.textContent === 'X') {
@@ -74,42 +74,47 @@ const employeesInfoRequest = (data) => {
 				})
 			}
 		})
-//The search-submit click event button
-		searchButton.addEventListener('click', (e) => {
-			if (searchInput.value === user.name.first || searchInput.value === user.name.last) {
-				containerDiv.style.display = 'flex';
-			} else {
-				containerDiv.style.display = 'none';
-			}
-		});
-//The event that restores the searched list when the search input is empty
-		body.addEventListener('keyup', () => {
-			const cards = document.querySelectorAll('.card');
-			if (searchInput.value === '') {
-				cards.forEach(card => {
-					card.style.display = 'flex';
-				})
-			}
-		});
 		return data;
 	})
 }
 //The function that handles the previous & next button on the modal window
 const modalInteraction = (data) => {
 	const modalContainers = document.querySelectorAll('.modal-container');
+	//The search-submit click event button
+	searchButton.addEventListener('click', (e) => {
+		e.preventDefault();
+		const cards = document.querySelectorAll('.card');
+		cards.forEach(card => {
+			card.style.display = 'none';
+			if (card.textContent.includes(searchInput.value.toLowerCase())) {
+				card.style.display = 'flex';
+			}
+		})
+	});
+	//The event that shows available employees as you type and restores the searched list when the search input is empty
+	body.addEventListener('keydown', () => {
+		const cards = document.querySelectorAll('.card');
+		cards.forEach(card => {
+			card.style.display = 'none';
+			if (card.textContent.includes(searchInput.value.toLowerCase())) {
+				card.style.display = 'flex';
+			}
+		})
+	});
+	//The prev & next button interactions
 	modalContainers.forEach((modal, i) => {
-//This "if" hides the previous button when its the 1st modal window
+		//This "if" hides the previous button when its the 1st modal window
 		if (i === 0) {
 			const children = modal.childNodes;
 			const prev = children[2].children[0];
 			prev.style.display = 'none';
-//This "else if" hides the next button when its the last modal window
+			//This "else if" hides the next button when its the last modal window
 		} else if (i === 11) {
 			const children = modal.childNodes;
 			const next = children[2].children[1];
 			next.style.display = 'none';
 		}
-//The click event of both the previous & next buttons
+		//The click event of both the previous & next buttons
 		modal.addEventListener('click', (e) => {
 			if (e.target.className === 'modal-prev btn') {
 				modal.style.display = 'none';
@@ -125,6 +130,4 @@ const modalInteraction = (data) => {
 }
 //Set the image below as my background image
 body.style.backgroundImage = "url('epicbackgroundimage.jpg')";
-fetchRequest(urlRequest)
-.then(employeesInfoRequest)
-.then(modalInteraction);
+fetchRequest(urlRequest).then(employeesInfoRequest).then(modalInteraction);
